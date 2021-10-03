ab229634b4ecd5ed9a2ff9df2d9556fd20b313da
Grete <greteveetla@gmail.com>
2021-10-03 23:00:48 +0300

    moved bgbtn to another folder, added onClick method, replaced "Theme.MaterialComponents.DayNight.NoActionBar" with "Theme.AppCompat.Light.NoActionBar"

--- app/src/main/java/com/sovellus/ilo/MainActivity.java
+++ app/src/main/java/com/sovellus/ilo/MainActivity.java
@@ -2,15 +2,38 @@ package com.sovellus.ilo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int i = 0;
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("onClick", "button clicked");
+                EditText editTxtName = (EditText) findViewById(R.id.editTxtName);
+                String name = editTxtName.getText().toString();
+
+                if (!name.isEmpty()) {
+                    //Intent intent = new Intent(this, HomeActivity.class);
+                    //intent.putExtra(EXTRA_TEXT, name);
+                    //startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Tarvitaan nimi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
